787509969d2cf268626187e01e4b7ede51913a14
Chris Matt <chooom@gmx.de>
2018-11-02 16:47:27 +0100

    style polishing

--- js/overlay.js
+++ js/overlay.js
@@ -9,19 +9,19 @@ function getOverlayHtml(content) {
 }
 // Fires randomly one of these text areas
 let dbsAlertMessages = [
-    "<h1><span>Read focused!</span></h1>" +
-    "Don`t waste your time looking up with stuff in the internet." +
-    "<br>" +
-    "Instead use your brain and spend your with more meanfull things :-)",
+        "<h1><span>Read focused!</span></h1>" +
+        "Don`t waste your time looking up with stuff in the internet." +
+        "<br>" +
+        "Instead use your brain and spend your with more meanfull things :-)",
 
-    "<h1><span>RTFM!</span></h1>" +
-    "Don`t be stupid fellow." +
-    "<br>" +
-    "Sometimes its helpfull to <b>R</b>ead <b>T</b>he <b>F</b>*king <b>M</b>anual :-)",
+        "<h1><span>R T F M !</span></h1>" +
+        "Don`t be stupid fellow." +
+        "<br>" +
+        "Sometimes its helpfull to <b>R</b>ead <b>T</b>he <b>F</b>*king <b>M</b>anual :-)",
 
-    "<h1><span>HAHAHAHA!</span></h1>" +
-    "Ouw! Its better spend my time on Social Media" +
-    "<br>"
+        "<h1><span>HAHAHAHA!</span></h1>" +
+        "Ouw! Its better spend my time on Social Media" +
+        "<br>"
 ]
 
 let style =
@@ -48,7 +48,7 @@ let style =
         "opacity: 1 !important;" +
         "color:black;margin-top: 1em;" +
         "font-size:26px;padding: 10px;" +
-        "transition: all .6s;" +
+        "transition: all .6s !important;" +
     "}" +
     "button:disabled {" +
         "opacity: .1 !important;" +
@@ -66,17 +66,17 @@ let style =
     "h1 span:after {" +
         "content: '';" +
         "position: absolute;" +
-        "height: 10px;" +
+        "height: 7px;" +
         "border-bottom: 1px solid white;" +
         "border-top: 1px solid white;" +
-        "top: 28px;" +
+        "top: 32px;" +
         "width:25%;" +
     "}" +
     "h1 span:before {" +
-        "right: 12em;" +
+        "right: 68%;" +
     "}" +
     "h1 span:after {" +
-        "left: 12em;" +
+        "left: 68%;" +
     "}"
 
 // Add a head style onto the overlayed body
