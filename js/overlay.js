836c7dc611625cd5c21a9659de44381b5a6090da
Chris Matt <chooom@gmx.de>
2018-11-09 13:00:30 +0100

    implemented all animation

--- js/overlay.js
+++ js/overlay.js
@@ -1,12 +1,11 @@
-function getOverlayHtml(logo, message, answer) {
+function getOverlayHtml(message, answer, question, voteBar, tags) {
     // Appends the following element into the body of stackoverflow
     let html =
-    "<header id='dbsNavbar' class='top-bar' style='border-top:unset;'>" +
+    "<header id='dbsNavbar' class='top-bar show' style='border-top:unset;'>" +
         "<div class='-container'>" +
             "<div class='dbsHeadOver'>" +
                 "<a href='#' class='left-sidebar-toggle p0 js-left-sidebar-toggle'>" +
                     "<a id='logoWrapper' href='#' title='comming soon - https://dont-be-stup.id'>" +
-                        "<img id='dbsLogo' src='"+ logo +"' />" +
                     "</a>" +
                     "<div id='drDoucheDialog'>" +
                         "<nav id='dialogMenu'></nav>" +
@@ -25,30 +24,27 @@ function getOverlayHtml(logo, message, answer) {
                 "<div class='inner-content clearfix'>" +
                     "<div id='question-header' class='grid'>" +
                         "<h1 itemprop='name' class='grid--cell fs-headline1 fl1'>" +
-                            "<div class='question-hyperlink'>" +
-
-                            "</div>" +
+                        question.title +
                         "</h1>" +
                         "<div class='pl8 aside-cta grid--cell' role='navigation' aria-label='ask new question'>" +
-                            "<a href='/questions/ask' class='d-inline-flex ai-center ws-nowrap s-btn s-btn__primary'>Ask Question</a>" +
+                            "<a href='/questions/ask' class='d-inline-flex ai-center ws-nowrap s-btn s-btn__primary'>Donate</a>" +
                         "</div>" +
                     "</div>" +
-                    "<div id='mainbar' role='main' style='background: white;'>" + // style='box-shadow: 9px 12px 28px rgba(55,55,55,0.1);'
+                    "<div id='mainbar' class='dbsMainbar' role='main'>" + // style='box-shadow: 9px 12px 28px rgba(55,55,55,0.1);'
                         "<div class='question' id='question'>" +
                             "<div class='post-layout'>" + 
-                                /* "<div class='votecell post-layout--left'>" +
+                                "<div class='votecell post-layout--left'>" +
                                     "<div class='vote'>" +
-                                        "<a class='vote-up-off'>up vote</a>" +
-                                        "<span class='vote-count-post'></span>" +
+                                        voteBar +
                                     "</div>" +
-                                "</div>" + */
+                                "</div>" +
                                 "<div class='postcell post-layout--right'>" +
                                     "<div class='post-text'>" +
                                         answer +
                                     "</div>" +
                                     "<div class='post-taglist grid gs4 gsy fd-column'>" +
                                         "<div class='grid ps-relative d-block'>" +
-                                            "<a href='/questions/tagged/html' class='post-tag js-gps-track' rel='tag'></a>" +
+                                        tags +
                                         "</div>" +
                                     "</div>" +
                                 "</div>" +
@@ -66,54 +62,63 @@ function getOverlayHtml(logo, message, answer) {
 }
 
 // Loops trough all anwers on the page and extracts the one with the highest votes.
-function getHighestVotedAnswer() {
+function getAnswers() {
     let allAnswers = document.querySelectorAll(".answer");
     if (allAnswers.length == 0) {
         return null;
     }
 
-    let result = {
-        voteCount: 0,
-        innerHTML: ''
-    };
-
-    allAnswers.forEach(o => {
-        let currentAnswer = o; 
-
-        let firstLevelDiv = currentAnswer.getElementsByTagName("div")[0];
+    let extractedAnswers = Array.from(allAnswers).map(answer => {
+        let firstLevelDiv = answer.getElementsByTagName("div")[0];
         let voteBarLeft = firstLevelDiv.getElementsByTagName("div")[1];
         let contentDiv = firstLevelDiv.getElementsByTagName("div")[2];
-
         let votes = voteBarLeft.getElementsByTagName("span")[0];
-        let voteNumber = parseInt(votes.innerText.trim());
+        let tagsArray = Array.from(answer.getElementsByClassName("post-tag"));
+        let voteCount = parseInt(votes.innerText.trim());
 
-        if (result.voteCount < voteNumber) {
-            result.voteCount = voteNumber;
-            result.innerHTML = contentDiv.children[0].innerHTML;
+        return {
+            classes: answer.classList,
+            innerHtml: contentDiv.children[0].innerHTML,
+            voteCount: voteCount,
+            voteBar: voteBarLeft.innerHTML,
+            tags: tagsArray.map(tag => tag.innerText)
+        };
+    });
+    
+    return extractedAnswers;
+}    
+
+function getHighestVotedAnswer() {
+    let allAnswers = getAnswers();
+    let maxVotedAnswer = {};
+
+    allAnswers.forEach(answer => {
+        if (!maxVotedAnswer.voteCount || answer.voteCount > maxVotedAnswer.voteCount) {
+            maxVotedAnswer = answer;
         }
-    }); 
-    return result;
+    });
+
+    return maxVotedAnswer;
 }
 
 function getAcceptedAnswer() {
-    let accepetedDomElement = document.querySelectorAll(".accepted-answer");
-    let result = {
-        voteCount: 0,
-        innerHTML: ''
-    };
-
-    if (accepetedDomElement.length == 0) {
+    let allAnswers = getAnswers();
+    let acceptedAnswers = allAnswers.filter(answer => answer.classes.contains("accepted-answer"));
+    if (acceptedAnswers.length == 0){
         return null;
-    } else {
-        return accepetedDomElement[0];
-    }
-    
+    }    
+    return acceptedAnswers[0];
 }
 
 function getQuestion() {
-    let getQuestionValue = document.getElementsByClassName("question-hyperlink");
-        gevoteCounttQuestionValue.innerHTML = "";
-}voteCount
+    let questionElement = document.getElementById("question-header")
+    let questionElementTitle = document.getElementsByClassName("question-hyperlink")[0];
+    let questionElementX = {
+        innerHTML: questionElement.innerHTML,
+        title: questionElementTitle.innerHTML
+    };
+    return questionElementX;
+}
 
 // Fires randomnly one of these text areas
 let dbsAlertMessages = [
@@ -146,9 +151,10 @@ let dbsAlertMessages = [
 
     "<div class='innerDialog'>" + 
         "<h1 class='dbsPushMessageTitle'>" +
-            "<a href='#>Muhaha ha ha!</a>" +
+            "<a href='#'>Muhaha ha ha!</a>" +
         "</h1>" +
-        "<p class='dbsPushMessage'>Ouw! Its better waste my time on Social Media</p>" +
+        "<p class='dbsPushMessage'>" +
+            "Ouw! Its better waste my time on Social Media</p>" +
     "</div>"
 ];
 
@@ -199,7 +205,7 @@ let style =
         "left: 208px;" +
         "right: 15%;" +
         "bottom: 0;" +
-        "z-index: -1;" +
+        "z-index: 0;" +
     "}" +
     "#dbsNavbar {" +
         "position: fixed;" +
@@ -208,8 +214,9 @@ let style =
         "transition: box-shadow cubic-bezier(.165, .84, .44, 1) .25s;" +
         "height: 51px;" +
         "box-sizing: border-box;" +
+        "opacity:1;" +
         "transition: all .4s;" +
-        "animation: animateNavbar .4s 1 alternate;" +
+        "animation: animateNavbar .2s 1 alternate;" +
     "}" +
     "#dbsLogo {" +
         "width: 90px;" +
@@ -223,23 +230,34 @@ let style =
         // background: url(https://cdn.dribbble.com/users/1065420/screenshots/3933364/gary-big-eyes.gif);" +
         // "background-size: 136% 89%;" +
         // "background-position: -6px -22px;" +
-    "}" +
+    "}" + 
     "#drDoucheDialog {" +
-        "animation: showDrDoucheDialog .3s 1 ease 1s forwards;" +
+        "max-height: 74px;" +
+        "min-height: 60px;" +
+        "animation: showDrDoucheDialog .3s 1 ease-in .6s forwards;" +
         "opacity: 0;" +
-        "transition: all .3s;" +
         "position: relative;" +
         "display: inline-flex;" +
         "padding: 0 5px 5px 5px;" +
         "word-break: break-all;" +
         "text-overflow: clip;" +
-        "margin-top: 5px;" +
         "left: 14px;" +
         "background: white;" +
         "border: 4px solid #333;" +
-        "width: 62.8%;" +
-        "max-height: 80px;" +
-        "min-height: 60px;" +
+    "}" +
+    "#acceptButton {" +
+        "padding: 0 15px;" +
+        "margin-top: 4px;" +
+        "letter-spacing: -1px;" +
+        "font-size: 23px;" +
+        "background: #333;" +
+        "border: 1px solid #999;" +
+        "box-shadow: unset;" +
+        "transition: all .3s;" +
+    "}" +
+    "#acceptButton:disabled {" +
+        "color: #FFF !important;" +
+        "opacity: .4;" +
     "}" +
     "#drDoucheDialog:after, #drDoucheDialog:before {" +
         "left: -19px;" +
@@ -264,7 +282,6 @@ let style =
         "border-width: 10px;" +
        " margin-top: -16px;" +
     "}" +
-    "}" +
     ".innerDialog {" +
         "line-height:30px;" +
     "}" +
@@ -275,37 +292,39 @@ let style =
         "width: 100%;" +
         "margin-top: 39px;" +
     "}" +
+    "#left-sidebar, #sidebar {" + 
+        "height: 84vh;" +
+        "transition: all .3s;" +
+        "animation: blurSidebars .6s 1 ease-out forwards;" +
+    "}" +
     ".dbsContainer {" +
         "top: 51px;" +
+        "box-sizing: border-box;" +
+        "overflow:hidden;" +
         "position: absolute;" +
-
+        "animation: animateOverlay 1s 1 alternate;" +
     "}" +
     ".dbsContainer #content {" +
         "padding:14px 24px 0 24px;" +
     "}" +
     "#dbsOverlay {" +
-        "animation: animateOverlay .6s 1 alternate .3s forwards;" +
-        "top: 23px;" +
         "position: relative;" +
-        "padding: 0 25px;" +
+        "top: 23px;" +
+        "transition: all .3s;" +
+        "padding: 0 24px;" +
     "}" +
     "#dbsOverlay .inner-content #question-header {" +
-        "margin: 0 auto 26px auto;" +
         "height: 49.5px;" +
         "background: white;" +
         "border-bottom: 1px solid #e4e6e8;" +
     "}" +
-    "#dbsOverlay .inner-content #mainbar {" +
-        "background: white;" +
+    ".dbsMainbar {" +
+        "padding-top:12px !important;" +
         "position: relative;" +
+        "background: linear-gradient(to bottom, rgba(255,255,255,1) 80%,rgba(255,255,255,0) 20%);" +
         "z-index: 1000;" +
-        "top: -12px;" +
-    "}" +
-    "#left-sidebar, #sidebar {" + 
-        "height: 84vh;" +
-        "filter: blur(7px);" +
-        "transition: all .3s;" +
-        "animation: blurSidebars .6s 1 ease-out;" +
+        "top: -13px;" +
+        "left: 1px;" +
     "}" +
     "#rollingEyes {" + 
         "width: 60px;" +
@@ -329,15 +348,14 @@ let style =
         "font-size: 38px !important;" +
     "}" +
     ".rotateEyeLeft {" + 
-        "animation: rotateLeft 2s 10 ease-out;" +
+        "animation: rotateLeft 2s 10 alternate 1s forwards;" +
     "}" +
     ".rotateEyeRight {" + 
-        "animation: rotateRight 2s 10 ease-out;"  +
+        "animation: rotateRight 2s 10 alternate .6s forwards;"  +
     "}" +
-    
     "@-webkit-keyframes blurSidebars {" +
-        "0% { -webkit-filter: blur(0); filter: translateZ(-100px); }" +
-        "100% { -webkit-filter: blur(7px); filter: translateZ(0); }" +
+        "0% { -webkit-filter: blur(0); }" +
+        "100% { -webkit-filter: blur(7px); }" +
     "}" +
     "@-webkit-keyframes animateOverlay {" +
         "0% { -webkit-filter: blur(5px); transform: translateY(-100%); }" +
@@ -362,6 +380,10 @@ let style =
     "@-webkit-keyframes rotateRight {" +
         "0% { transform: rotate(0deg); }" +
         "100% { transform: rotate(-359deg); }" +
+    "}" +
+    "@-webkit-keyframes changeDoucheDialogContent {" +
+        "0% { opacity: 0; }" +
+        "100% { opacity: 1; }" +
     "}";
 
 // Builds the Random Facelogo Ambient    
@@ -376,9 +398,8 @@ let logoImagePaths = [
 
 // Multiplies a random number with the array-length of the alert messages and logo type
 let randomMessagesIndex = Math.floor(Math.random() * Math.floor(dbsAlertMessages.length));
-let randomLogoTypeIndex = Math.floor(Math.random() * Math.floor(logoImagePaths.length));
+let randomLogoPathIndex = Math.floor(Math.random() * Math.floor(logoImagePaths.length));
 // Choose randomly one of the thre text areas
-let randomMessage = dbsAlertMessages[randomMessagesIndex]; 
 
 // Fires the highest voted answers if no accepted answer avaiable
 let highestVotedAnswer = getHighestVotedAnswer();
@@ -388,7 +409,7 @@ let accepetedDomElement = getAcceptedAnswer();
 
 // Proofes whether the Objects are correctly setted
 let displayAnswer = accepetedDomElement;
-if (!accepetedDomElement) {
+if (!accepetedDomElement || accepetedDomElement.voteCount < highestVotedAnswer.voteCount) {
     displayAnswer = highestVotedAnswer;
 }
 
@@ -403,14 +424,69 @@ let styleElement = document.createElement("style"); // Add a head style onto the
     styleElement.appendChild(document.createTextNode(style));
     document.getElementsByTagName("head")[0].appendChild(styleElement);
 
+function getRandomMessage() {
+    let RandomMessageObj = dbsAlertMessages[randomMessagesIndex];
+    return RandomMessageObj;
+}
+
+// CALL ME CONSTRUCTOR :-P
 // Pushes the fired objects frome the functions into the mixed frontend body
-let overlayHtml = getOverlayHtml(logoImagePaths[randomLogoTypeIndex], randomMessage, displayAnswer.innerHTML);
+let overlayHtml = getOverlayHtml(getRandomMessage(), displayAnswer.innerHtml, getQuestion(), displayAnswer.voteBar, displayAnswer.tags);
     document.body.innerHTML += overlayHtml;
 
-if (randomLogoTypeIndex == 1) {
-    let logoLinkedWrapper = document.getElementById("logoWrapper");
+// Main Selector of the overlayerd dbs content
+let dbsContainer = document.getElementsByClassName("dbsContainer")[0];
+// Selects dr`s douche`s dialog
+let dialogSelector = document.getElementById("drDoucheDialog");
+    dialogSelector.style.minWidth = "794px";
+
+function dialogWidth() {
+    dialogSelector.style.minWidth = "40px";
+    dialogSelector.style.transition = "all .2s";
+    return;    
+}
+
+// Close the dialog inside navigation after 7 seconds
+setTimeout(function() {
+    dialogSelector.innerHTML = "";
+    let closeButton = dbsCloseButton();
+    dialogWidth();
+    dialogSelector.appendChild(closeButton);    
+}, 5000); 
+
+// Sets the Height of the overlay parent div
+function copyFromOrgPage() {
+    let mainBarEl = document.getElementById("mainbar");
+    let questHead = document.getElementById("question-header");
+        questHead.style.display = "none";
+    let dbsMainbar = document.getElementsByClassName("dbsMainbar");
+        dbsMainbar[0].style.height = mainBarEl.offsetHeight + 'px';
+    return dbsMainbar;
+}
+
+// Creates the logo element
+copyFromOrgPage();
+let logoImageOrg = 'https://diekommune.de.cool/0.svg';
+let logoWrapper = document.getElementById("logoWrapper");
+let logoImagePath = logoImagePaths[randomLogoPathIndex];
+let logoImgElement = document.createElement("img");
+    logoImgElement.id = "dbsLogo";
+    logoImgElement.src = logoImagePath;
+    let i = 0;
+    logoImgElement.onclick = (logoImagePath) => {   
+        let url = logoImagePaths[i];
+        document.getElementById('dbsLogo').src = url;
+        if (i == logoImagePaths.length -1) {
+            i = 0;
+            return;
+        }
+        i = i + 1;
+    };
+    logoWrapper.appendChild(logoImgElement);
+
+if (randomLogoPathIndex == 1) {
     let eyeWrapper = buildRollEyes();
-    logoLinkedWrapper.appendChild(eyeWrapper);
+    logoWrapper.appendChild(eyeWrapper);
 }
 
 function buildRollEyes() {
@@ -420,32 +496,57 @@ function buildRollEyes() {
         "<span class='ball rotateEyeLeft'>.</span>" +
         "<span class='ball rotateEyeRight'>.</span>";
     setTimeout(function(){ 
-        eyeWrapper.style.display = 'block';
+        eyeWrapper.style.display = "block";
     }, 200);
     return eyeWrapper;
-} 
+}
 
-// Delete original question-header
-// Deactive-Button dbsButton document.getElementById("acceptButton").disabled
+let dbsNavbar = document.getElementById("dbsNavbar");
+
+// Creates the close button
 function dbsCloseButton() {
-    // Deactivates the button and counts to 0
+    let dbsButton = document.createElement("button");
     let counter = 5;
+        dbsButton.id = "acceptButton";
+        dbsButton.innerHTML = counter;
+        dbsButton.disabled = true;
+        // Close the overlayered box over the open tab
+        dbsButton.onclick = (e) => {
+            // Selects the navigation header
+                dbsNavbar.className = dbsNavbar.className !== "top-bar show" ? "top-bar show" : "top-bar hide";
+            let dbsNavStyle = dbsNavbar.style;
+
+            if(dbsNavbar.className == "top-bar show"){
+                dbsNavStyle.display = "block";
+                dbsNavStyle.transform = "translateY(0)";
+                window.setTimeout(function(){
+                    dbsNavStyle.opacity = 1;
+                },0);
+              }
+            if (dbsNavbar.className === "top-bar hide") {
+                dbsNavStyle.transform = "translateY(-90px)";
+                dbsNavStyle.opacity = 0;
+                dbsContainer.style.opacity = 0;
+                window.setTimeout(function(){
+                    dbsNavStyle.display = "none";
+                },4000); // timed to match animation-duration
+            }
+        };
+
+    // Deactivates the button and counts to 0
+        counter = 4;
     let interval = setInterval(o => {
         dbsButton.innerHTML = counter;
         counter--;
         if (counter == 0) {
-            clearInterval(timer);
+            clearInterval(interval);
             dbsButton.innerHTML = "okay, got it!";
         }
     }, 1000);
 
     // Deactivates the button for 5 seconds
     setTimeout(function () {
-        document.getElementById("acceptButton").disabled = false;
-    }, 5000);
-
-    // Close the overlay box over the open tab
-    dbsButton.onclick = (e) => {
-        dbsOverlay.remove();
-    };
+        dbsButton.disabled = false;
+    }, 100);
+    return dbsButton;
 }
